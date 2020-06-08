86ce3c7fae9cd88449bfc8a79e06d0d30dbb648e
RICH <manukyan231@mail.ru>
2020-06-08 20:57:44 +0400

    start commit

---
+++ src/main/java/am/basic/jdbcStart/util/constants/Pages.java
@@ -0,0 +1,17 @@
+package am.basic.jdbcStart.util.constants;
+
+public interface Pages {
+
+
+    String HOME_PAGE = "/secure/home.jsp";
+
+    String INDEX_PAGE = "/index.jsp";
+
+    String VERIFICATION_PAGE = "/verification.jsp";
+
+    String REGISTER_PAGE = "/register.jsp";
+
+    String FORGET_PASSWORD_PAGE = "/forget-password.jsp";
+
+    String RECOVER_PASSWORD_PAGE = "/recover-password.jsp";
+}
